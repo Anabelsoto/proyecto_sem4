cb9a4c0dd1336025ce0520ce811b7bef27e5ad39
Anabelsoto <77157596+Anabelsoto@users.noreply.github.com>
2021-09-01 21:15:42 -0500

    primer cambio

---
+++ app/Compra_detalle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Compra_detalle extends Model
+{
+    //
+    public $table ='compra_detalle';
+    protected $primarykey ='idcompra_detalle';
+}
---
+++ app/Venta_detalle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venta_detalle extends Model
+{
+    //
+    public $table ='venta_detalle';
+    protected $primarykey ='idventa_detalle';
+}
